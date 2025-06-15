28d0f0e9a69ede8f2c6b05798a66ce69e25f6ce0
matej2005 <68588948+matej2005@users.noreply.github.com>
2025-06-15 18:36:35 +0200

    updated speed number to string

--- main.ts
+++ main.ts
@@ -58,14 +58,14 @@ namespace Robotrix {
 
     //% blockId=robotrix_expander_move
     //% block="move|in|direction $d at|speed $speed"
-    export function carMoveSimple(d: directions, speed: number = 150): void {
-        let a = 0x00;
+    export function carMoveSimple(d: directions, speed: string = "A0"): void {
+        let a = "0x00";
         switch (d) {
             case directions.STOP:
-                a = 0x00;
+                a = "0x00";
                 break;
             case directions.FORWARD:
-                a = 0x01;
+                a = "0x01";
                 break;
         }
         sendDataToExpander("0x20" + a + speed + "0x00");
